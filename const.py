03b870df477a9ad627304f82e128110f2490ec03
beatriz <44908994+bvechiato@users.noreply.github.com>
2023-11-02 18:15:17 +0000

    Update const.py

--- const.py
+++ const.py
@@ -5,17 +5,18 @@ import getpass
 CUSTOM_CHAT = False
 
 ## buffer for socket
-BUFFER_DIR = Template('/tmp/$usr').substitute(usr = getpass.getuser())
+# The slash here is important...
+BUFFER_DIR = Template('/tmp/$usr/').substitute(usr = getpass.getuser())
 BUFFER_FILE_NAME = 'buffer'
 
 ## Secret messages
 NICE_MSG = {
-    'alice': 'I love you!',
-    'bob': 'I love you too!'
+    'alice': 'I love you too!',
+    'bob': 'I love you!'
 }
 BAD_MSG = {
-    'alice': 'I hate you!',
-    'bob': 'You broke my heart...'
+    'alice': 'You broke my heart...',
+    'bob': 'I hate you!'
 }
 
 ## Coloured output
@@ -27,3 +28,4 @@ FAIL = '\033[91m'
 ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
+
