74549662c687be6970705bd0852194d2dc82c2cd
beatriz <44908994+bvechiato@users.noreply.github.com>
2023-11-02 18:14:41 +0000

    Update bob.py

--- bob.py
+++ bob.py
@@ -1,26 +1,21 @@
 import sys
+import os
 
 from common import *
 from const import *
 
 dialog = Dialog('print')
-player = sys.argv[0].split('.', 1)[0]
+player = os.path.basename(sys.argv[0]).split('.', 1)[0]
 socket, aes = setup(player, BUFFER_DIR, BUFFER_FILE_NAME)
 
-dialog.info("Waiting for message...")
-received = receive_and_decrypt(aes, socket)
-dialog.chat('Alice said: "{}"'.format(received))
 if CUSTOM_CHAT:
     dialog.prompt('Please input message...')
     to_send = input()
 else:
-    if received == NICE_MSG['alice']:
-        to_send = NICE_MSG[player]
-    elif received == BAD_MSG['alice']:
-        to_send = BAD_MSG[player]
-    else:
-        to_send = 'What?!'
+    to_send = NICE_MSG[player]
 encrypt_and_send(to_send, aes, socket)
-dialog.info('Message sent!')
+dialog.info('Message sent! Waiting for reply...')
+received = receive_and_decrypt(aes, socket)
+dialog.chat('Alice said: "{}"'.format(received))
 
 tear_down(socket, BUFFER_DIR, BUFFER_FILE_NAME)
