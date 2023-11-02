0f337b92aaf0f982da1dfdb5b4f3efc3950f7163
beatriz <44908994+bvechiato@users.noreply.github.com>
2023-11-02 18:10:14 +0000

    Update alice.py

--- alice.py
+++ alice.py
@@ -1,20 +1,27 @@
 import sys
+import os
 
 from common import *
 from const import *
 
 dialog = Dialog('print')
-player = sys.argv[0].split('.', 1)[0]
+player = os.path.basename(sys.argv[0]).split('.', 1)[0]
 socket, aes = setup(player, BUFFER_DIR, BUFFER_FILE_NAME)
 
+dialog.info("Waiting for message...")
+received = receive_and_decrypt(aes, socket)
+dialog.chat('Bob said: "{}"'.format(received))
 if CUSTOM_CHAT:
     dialog.prompt('Please input message...')
     to_send = input()
 else:
-    to_send = NICE_MSG[player]
+    if received == NICE_MSG['bob']:
+        to_send = NICE_MSG[player]
+    elif received == BAD_MSG['bob']:
+        to_send = BAD_MSG[player]
+    else:
+        to_send = 'What?!'
 encrypt_and_send(to_send, aes, socket)
-dialog.info('Message sent! Waiting for reply...')
-received = receive_and_decrypt(aes, socket)
-dialog.chat('Bob said: "{}"'.format(received))
+dialog.info('Message sent!')
 
 tear_down(socket, BUFFER_DIR, BUFFER_FILE_NAME)
